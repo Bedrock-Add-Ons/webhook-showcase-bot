a8f4f69b48f12e7521eb1f5ba5aed1c36ac93821
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2022-03-01 22:16:54 +0000

    Updated to discordjs v13, and added idea from issue#1

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
-// require dependencies
-const Discord = require("discord.js")
-const config = require('./config.json');
+const { Client, Intents, WebhookClient } = require('discord.js')
 
-// set up discord client
-const client = new Discord.Client()
-client.login(config.token)
-
-// set up webhook
-let splitWebhookURL = config.discussion_webhook.split("/")
-const webhookToken = splitWebhookURL.pop()
-const webhookID = splitWebhookURL.pop()
-const creationsWebhook = new Discord.WebhookClient(webhookID, webhookToken)
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const config = require('./config.json')
+const creationsWebhook = new WebhookClient({ "url": config.discussion_webhook })
 
 // log when bot is running
 client.once("ready", () => console.log("Ready"))
 
-// move messages
-client.on("message", message => {
+// copy messages from showcase to discussion
+client.on("messageCreate", async message => {
+
 	if (message.channel.id != config.showcase_id) return
 	if (message.member.nickname == null) membername = message.author.username
 	else membername = message.member.nickname
-	if (message.attachments.array().length > 0) {
-		let url = []
-		message.attachments.forEach(attachment => {
-			const ImageLink = attachment.proxyURL;
-			url.push(ImageLink)
-		});
-		try {
-			creationsWebhook.send(message.content, { "username": membername, "files": url, "avatarURL": message.author.avatarURL({ dynamic: true }), allowedMentions: { parse: [] } })
-		} catch {
-			creationsWebhook.send(`${message.content} \n ${message.url}`, { "username": membername, "avatarURL": message.author.avatarURL({ dynamic: true }), allowedMentions: { parse: [] } })
-		}
-	}
-	else {
-		creationsWebhook.send(message.content, { "username": membername, "avatarURL": message.author.avatarURL({ dynamic: true }), allowedMentions: { parse: [] } })
-	}
-	message.react("👍"); // this is optional, adds an emoji as a reaction in the showcase channel
+
+	const webhookData = { "content": message.content, "username": membername, "files": [], "avatarURL": message.author.avatarURL({ dynamic: true }), "allowedMentions": { parse: [] } }
+	message.attachments.forEach(attachment => {
+		const ImageLink = attachment.proxyURL
+		webhookData.files.push(ImageLink)
+	})
+	const messageObject = await creationsWebhook.send(webhookData)
 	console.log("Showcase sent by " + message.author.username + '#' + message.author.discriminator + " in " + message.channel.name + ": " + message.content)
+
+	setTimeout(async () => {
+		try {
+			await message.react("👍");
+		} catch (err) {
+			console.log(messageObject.id)
+			creationsWebhook.deleteMessage(messageObject.id)
+		}
+	}, 6000)
 })
+
+client.login(config.token)
